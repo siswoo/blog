f103d2c8954c7c39c29f3c29b7462af0c294d721
siswoo <55102664+siswoo@users.noreply.github.com>
2021-03-05 10:44:37 -0500

    Primer Commit

---
+++ header.php
@@ -0,0 +1,33 @@
+<div class="container mt-5">
+	<div class="row">
+		<div class="col-4 text-center">
+			LOGO
+		</div>
+		<div class="col-4 text-center">
+			<input type="search" name="buscador1" id="buscador1" class="form-control" placeholder="Buscador" style="display: inline; width: 200px;">
+			<i class="fas fa-search ml-3" style="cursor: pointer;"></i>
+		</div>
+		<div class="col-4 text-center">
+			<i class="fab fa-facebook ml-3" style="font-size: 40px;"></i>
+			<i class="fab fa-twitter-square ml-3" style="font-size: 40px;"></i>
+			<i class="fab fa-instagram-square ml-3" style="font-size: 40px;"></i>
+		</div>
+	</div>
+	<div class="row mt-4">
+		<div class="col-2 text-center">
+			<div>INICIO</div>
+		</div>
+		<div class="col-2 text-center">
+			<div>SATELITES</div>
+		</div>
+		<div class="col-2 text-center">
+			<div>COMIENZO</div>
+		</div>
+		<div class="col-2 text-center">
+			<div>JUGUETES</div>
+		</div>
+		<div class="col-2 text-center">
+			<div>SENSUALIDAD</div>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href="resources/fontawesome/css/all.css" rel="stylesheet">
+</head>
+<body>
+<?php
+include('header.php');
+?>
+</body>
+</html>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
